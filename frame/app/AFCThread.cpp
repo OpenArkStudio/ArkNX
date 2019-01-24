4410e298d0b177de2440a8a04cb8af1993410f1e
freeeyes <freeeyes@163.com>
2019-01-24 16:09:10 +0800

    add thread logic code design.

--- frame/app/AFCThread.cpp
+++ frame/app/AFCThread.cpp
@@ -1,21 +1,41 @@
 #include "AFCThread.h"
 
 
-ark::AFCThread::AFCThread() : thread_id_(HANDEL_ERROR_VALUE)
+ark::AFCThread::AFCThread() : thread_id_(HANDEL_ERROR_VALUE), thread_mutex_(NULL), thread_cond_(NULL)
 {
-
+#if ARK_PLATFORM == PLATFORM_WIN
+    thread_mutex_ = new CRITICAL_SECTION();
+    thread_cond_ = new CONDITION_VARIABLE();
+#else
+    thread_mutex_ = new pthread_mutex_t();
+    thread_cond_ = new pthread_cond_t();
+#endif
 }
 
 ark::AFCThread::~AFCThread()
 {
+    if (NULL != thread_mutex_)
+    {
+        delete thread_mutex_;
+        thread_mutex_ = NULL;
+    }
 
+    if (NULL != thread_cond_)
+    {
+        delete thread_cond_;
+        thread_cond_ = NULL;
+    }
 }
 
-bool ark::AFCThread::CreateThread(ThreadCallbackRun thread_callback_run, void* arg)
+bool ark::AFCThread::CreateThread(ThreadCallbackLogic thread_callback_logic, void* arg)
 {
+    thread_param_.thread_                = (AFIThread* )this;
+    thread_param_.arg_                   = arg;
+    thread_param_.thread_callback_logic_ = thread_callback_logic;
+
 #if ARK_PLATFORM == PLATFORM_WIN
     unsigned int thread_id = 0;
-    thread_id_ = (HANDLE)_beginthreadex(NULL, 0, thread_callback_run, (PVOID)arg, 0, &thread_id);
+    thread_id_ = (HANDLE)_beginthreadex(NULL, 0, ThreadCallbackRun, (PVOID)&thread_param_, 0, &thread_id);
 #else
     pthread_create(&thread_id_, NULL, ThreadCallbackRun, (void*)&arg);
 #endif
@@ -70,6 +90,58 @@ bool ark::AFCThread::IsAlive()
     {
         return false;
     }
+}
+
+bool ark::AFCThread::Pause()
+{
+#if ARK_PLATFORM == PLATFORM_WIN
+    SleepConditionVariableCS(reinterpret_cast<PCONDITION_VARIABLE>(thread_cond_),
+                             reinterpret_cast<PCRITICAL_SECTION>(thread_mutex_),
+                             INFINITE);
+#else
+    pthread_cond_wait(thread_cond_,
+                      thread_mutex_);
+#endif
+    return true;
+}
+
+bool ark::AFCThread::Resume()
+{
+#if ARK_PLATFORM == PLATFORM_WIN
+    LeaveCriticalSection(thread_mutex_);
+#else
+    pthread_mutex_unlock(thread_mutex_);
+#endif
+
+    return true;
+}
+
+void ark::AFCThread::Lock()
+{
+#if ARK_PLATFORM == PLATFORM_WIN
+    EnterCriticalSection(thread_mutex_);
+#else
+    pthread_mutex_lock(thread_mutex_);
+#endif
+}
+
+void ark::AFCThread::UnLock()
+{
+#ifdef WIN32
+    LeaveCriticalSection(thread_mutex_);
+#else
+    pthread_mutex_unlock(thread_mutex_);
+#endif
+}
+
+void ark::AFCThread::SaveLastRunTimeBegin()
+{
+    //save thread last run time begin
+}
+
+void ark::AFCThread::SaveLastRunTimeEnd()
+{
+    //save thread last run time End
 }
 
 ark::ThreadID ark::AFCThread::GetThreadID()
--- frame/app/AFCThread.h
+++ frame/app/AFCThread.h
@@ -41,32 +41,120 @@ using namespace std;
 
 namespace ark
 {
+    //Thread return
+    enum ThreadReturn
+    {
+        ARK_THREAD_RETURN_ONCE = 0,
+        ARK_THREAD_RETURN_CONTINUE,
+        ARK_THREAD_RETURN_ERROR,
+    };
+
+    //thread logic function
+    typedef ThreadReturn (*ThreadCallbackLogic)(void*);
+
 #if ARK_PLATFORM == PLATFORM_WIN
-    typedef unsigned (*ThreadCallbackRun)(LPVOID);
     typedef HANDLE ThreadID;
+    typedef CRITICAL_SECTION ThreadMutex;
+    typedef CONDITION_VARIABLE ThreadCond;
 #define HANDEL_ERROR_VALUE INVALID_HANDLE_VALUE
 #else
-    typedef void* (*ThreadCallbackRun)(void*);
     typedef pthread_t ThreadID;
+    typedef pthread_mutex_t ThreadMutex;
+    typedef pthread_cond_t ThreadCond;
 #define HANDEL_ERROR_VALUE -1
 #endif
 
-    class AFCThread
+    class AFIThread
+    {
+    public:
+        AFIThread() {};
+        virtual ~AFIThread() = 0;
+
+        virtual void Lock() = 0;
+
+        virtual void UnLock() = 0;
+
+        virtual void SaveLastRunTimeBegin() = 0;
+
+        virtual void SaveLastRunTimeEnd() = 0;
+    };
+
+    //thread param
+    class AFCThreadParam
+    {
+    public:
+        AFCThreadParam() : thread_(NULL), arg_(NULL) {};
+
+    public:
+        AFIThread* thread_;
+        void*      arg_;
+        ThreadCallbackLogic thread_callback_logic_;
+    };
+
+    //run thread logic
+#if ARK_PLATFORM == PLATFORM_WIN
+    unsigned ThreadCallbackRun(void* arg)
+#else
+    void* ThreadCallbackRun(void* arg)
+#endif
+    {
+        AFCThreadParam* thread_param = (AFCThreadParam* )arg;
+
+        thread_param->thread_->Lock();
+
+        while (true)
+        {
+            thread_param->thread_->SaveLastRunTimeBegin();
+            ThreadReturn thread_return = thread_param->thread_callback_logic_(thread_param->arg_);
+            thread_param->thread_->SaveLastRunTimeEnd();
+
+            if (ARK_THREAD_RETURN_ONCE == thread_return)
+            {
+                //thread logic just run once
+                break;
+            }
+            else if (ARK_THREAD_RETURN_ERROR == thread_return)
+            {
+                //write log
+            }
+        }
+
+        thread_param->thread_->UnLock();
+
+        return 0;
+    };
+
+    class AFCThread : public AFIThread
     {
     public:
         AFCThread();
-        ~AFCThread();
+        virtual ~AFCThread();
 
-        bool CreateThread(ThreadCallbackRun thread_callback_run, void* arg);
+        bool CreateThread(ThreadCallbackLogic thread_callback_logic, void* arg);
 
         int KillThread();
 
         bool IsAlive();
 
+        bool Pause();
+
+        bool Resume();
+
+        virtual void Lock();
+
+        virtual void UnLock();
+
+        virtual void SaveLastRunTimeBegin();
+
+        virtual void SaveLastRunTimeEnd();
+
         ThreadID GetThreadID();
 
     private:
         ThreadID thread_id_;
+        ThreadMutex* thread_mutex_;
+        ThreadCond* thread_cond_;
+        AFCThreadParam thread_param_;
     };
 }
 
